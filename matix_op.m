ef435b3fbadaa72a842d97a4f3d11d3009449634
NikhitaSodisetti <nikhita.sodisetti.14@gmail.com>
2025-05-12 11:53:23 +0530

    Add files via upload

---
+++ matix_op.m
@@ -0,0 +1,17 @@
+clc;
+clear;
+close all;
+
+x=[1 2;3 4] % 2x2 Matrix x
+y=[5 6;7 8] % 2x2 Matrix y
+
+add_s=x+5 % Scalar Addition
+add=x+y % Matrix Addition
+sub_s=x-1 % Scalar Subtraction
+sub=y-x % Matrix Subtraction
+mult_s=x*2 % Scalar Multiplication
+mult=x.*y % Matrix Multiplication (element-wise)
+div_s=x/2 % Scalar Division
+div=y/x % Matrix Division
+tr=x' % Matrix Transpose
+
